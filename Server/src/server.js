f0193440c38fa91c42b708a42ef71d98654760c8
Raman <bohtraman@gmail.com>
2026-02-14 17:28:56 +0530

    "some bugs edit or solved"

--- Server/src/server.js
+++ Server/src/server.js
@@ -5,8 +5,18 @@ import ConnectDB from './config/db.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json(
+  {limit: '50kb',
+  extended: true,
+}
+));
+app.use(express.urlencoded({limit: '50kb', extended: true}));
+app.use(express.static('public'));
+app.use(cors({
+  origin: '*',
+  credentials: true,
+
+}));
 ConnectDB();
 
 
