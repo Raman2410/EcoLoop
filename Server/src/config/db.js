b41e0fbcfa87ab561ba4cde08229af6745782b7c
Raman <bohtraman@gmail.com>
2026-02-15 09:45:50 +0530

    file structure and db improve

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const ConnectDB = async()=>{
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`);
     console.log(`MongoDB connected: successfully`);
   } catch (error) {
  console.error("MongoDB connection failed:", error.message);
